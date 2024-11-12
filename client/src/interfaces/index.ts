6a9ee1d6bdbd3841b3f80ca8f549e5ac27d4b07a
kforsl <forslund95@live.com>
2024-11-12 12:31:29 +0100

    MenuItem interface

---
+++ client/src/interfaces/index.ts
@@ -0,0 +1,31 @@
+export interface MenuItemType {
+    _id: string;
+    name: string;
+    description: string;
+    type: string;
+    imageUrl: string;
+    sizes: SizeType[];
+}
+
+interface SizeType {
+    size: string;
+    price: number;
+    ingredients: IngredientType[];
+}
+
+interface IngredientType {
+    ingredientItem: IngredientItemType;
+    quantity: number;
+}
+
+interface IngredientItemType {
+    _id: string;
+    name: string;
+    description: string;
+    allergens: AllergensType[];
+}
+
+interface AllergensType {
+    type: string;
+    description: string;
+}
