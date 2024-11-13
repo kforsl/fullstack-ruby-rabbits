f5cfd933120134dec4cbc4f351b1760c7fbfc972
Jaerker <johanjakberger@gmail.com>
2024-11-13 10:46:46 +0100

    setup for starting

--- client/src/pages/AboutUsPage/AboutUsPage.tsx
+++ client/src/pages/AboutUsPage/AboutUsPage.tsx
@@ -1,6 +1,14 @@
 import './aboutUsPage.css';
+import waves from '../../../public/images/layered-waves-haikei.svg';
+
 const AboutUsPage: React.FC = () => {
-    return <div>AboutUsPage</div>;
+    return (<>
+        <main className='menu-page'>
+        <img className='menu-page__bg' src={waves} alt='melting ice cream background' />
+        
+    
+        </main>
+    </>)
 };
 
 export default AboutUsPage;
