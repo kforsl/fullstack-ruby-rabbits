3216c5c8d62afd2f6f616d4d4775ade82a5ae339
kforsl <forslund95@live.com>
2024-11-18 15:26:53 +0100

    base

--- business/src/App.tsx
+++ business/src/App.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from 'react';
 import RoutesComponent from './routes/RoutesComponent';
+import { socket } from './services/webSocket/ioSocket';
+
+async function socketIoConnect() {
+    socket.emit('connection');
+}
 
 function App() {
+    useEffect(() => {
+        socketIoConnect();
+    }, []);
     return (
         <>
             <RoutesComponent />
--- server/server.js
+++ server/server.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { createServer } = require('node:http'); // socket.io
+const { Server } = require('socket.io'); // socket.io
+var cors = require('cors');
 const app = express();
+
 require('dotenv').config();
+app.use(cors());
+const server = createServer(app); // socket.io
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    },
+});
 
 const menuRoute = require('./routes/menuRoute');
 const ingredientRoute = require('./routes/ingredientRoute');
@@ -19,13 +31,24 @@ app.use('/api/allergens', allergenRoute);
 
 const run = async () => {
     try {
-      await mongoose.connect(process.env.DB_URL);
-      app.listen(3000, () => console.log(`Server started on PORT ${PORT}`));
+        await mongoose.connect(process.env.DB_URL);
+        io.on('connection', (socket) => {
+            console.log('a user connected');
+            socket.on('disconnect', async (reason) => {
+                console.log(`USER DISCONNECTED: ${socket.id}. REASON: ${reason}`);
+            });
+            socket.on('createOrder', () => {
+                console.log('createOrder');
+            });
+            socket.on('updateOrderStatus', () => {
+                console.log('updateOrderStatus');
+            });
+        });
+        server.listen(3000, () => console.log(`Server started on PORT ${PORT}`));
     } catch (error) {
-      console.error(error);
-      process.exit(1);
+        console.error(error);
+        process.exit(1);
     }
-  };
-
+};
 
 run();
