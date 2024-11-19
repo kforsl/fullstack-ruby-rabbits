2ce0980a7abb8e8028526b29b16dd2e46ac88f9a
Jaerker <johanjakberger@gmail.com>
2024-11-19 11:08:05 +0100

    saving before gettin env variables

--- server/models/allergenModel.js
+++ server/models/allergenModel.js
@@ -1,4 +1,4 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const AllergenSchema = new Schema({
     type: {
@@ -7,10 +7,10 @@ const AllergenSchema = new Schema({
     },
     description: {
         type: String,
-        required: false
-    }
+        required: false,
+    },
 });
 
 const AllergenModel = model('Allergen', AllergenSchema);
 
-module.exports = {AllergenSchema, AllergenModel}
+module.exports = { AllergenSchema, AllergenModel };
