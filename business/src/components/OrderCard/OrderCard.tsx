e4aacac830b3d1b74869873500bd1d9ed1fa52ce
Magnus Nilsson <regnhundar@gmail.com>
2024-11-19 16:38:20 +0100

    Layout for ordercard s, m and l done. Chefpage layout done.

--- business/src/components/OrderCard/OrderCard.tsx
+++ business/src/components/OrderCard/OrderCard.tsx
@@ -4,34 +4,98 @@ import TextButton from '../TextButton/TextButton';
 import './orderCard.css';
 interface Props {
     size: 'small' | 'medium' | 'large';
-    order: OrderType;
+    order?: OrderType;
 }
+
 const OrderCard: React.FC<Props> = ({ size, order }) => {
-    const calculateAmountInOrder = (): number =>
-        order.order.reduce((totalAmount: number, orderItem: OrderItemType) => totalAmount + orderItem.quantity, 0);
+    // const calculateAmountInOrder = (): number =>
+    //     order.order.reduce((totalAmount: number, orderItem: OrderItemType) => totalAmount + orderItem.quantity, 0);
 
     if (size === 'small')
         return (
             <li className={`order-card order-card--small`}>
-                <h3 className='order-card__order-id'>{getLastNCharacters(order._id, 4)}</h3>
+                <h3 className='order-card__order-id'>44QR</h3>
+                <h4 className='order-card__time'>15:42</h4>
+                {/* <h3 className='order-card__order-id'>{getLastNCharacters(order._id, 4)}</h3>
+                <h4 className='order-card__time'>{formatDate(order.createdAt)}</h4> */}
             </li>
         );
     if (size === 'medium')
         return (
             <li className='order-card order-card--medium'>
-                <h3 className='order-card__order-id'>{getLastNCharacters(order._id, 4)}</h3>
+                <h3 className='order-card__order-id'>44QR</h3>
+                <h3 className='order-card__date'>15:42</h3>
+                <h3 className='order-card__amount'>
+                    <span>Antal:</span>
+                    <span>2</span>
+                </h3>
+                {/* <h3 className='order-card__order-id'>{getLastNCharacters(order._id, 4)}</h3>
                 <h3 className='order-card__date'>{formatDate(order.createdAt)}</h3>
                 <h3 className='order-card__amount'>
                     <span>Antal:</span>
                     <span>{calculateAmountInOrder()}</span>
-                </h3>
+                </h3> */}
                 <TextButton onClick={() => console.log('Här var det klickat!')}>TILLAGA</TextButton>
             </li>
         );
     if (size === 'large')
         return (
             <ul className='order-card order-card--large'>
-                <h3 className='order-card__order-id'>{getLastNCharacters(order._id, 4)}</h3>
+                <h3 className='order-card__order-id'>44QR</h3>
+                <h3 className='order-card__date'>15:42</h3>
+
+                <li className='order-card__order-item'>
+                    <h4 className='order-card__product-name'>produktnamn</h4>
+                    <h4 className='order-card__product-size'>size</h4>
+                    <h4 className='order-card__product-quantity'>2 st</h4>
+                </li>
+                <li className='order-card__order-item'>
+                    <h4 className='order-card__product-name'>produktnamn</h4>
+                    <h4 className='order-card__product-size'>size</h4>
+                    <h4 className='order-card__product-quantity'>2 st</h4>
+                </li>
+                <li className='order-card__order-item'>
+                    <h4 className='order-card__product-name'>produktnamn</h4>
+                    <h4 className='order-card__product-size'>size</h4>
+                    <h4 className='order-card__product-quantity'>2 st</h4>
+                </li>
+                <li className='order-card__order-item'>
+                    <h4 className='order-card__product-name'>produktnamn</h4>
+                    <h4 className='order-card__product-size'>size</h4>
+                    <h4 className='order-card__product-quantity'>2 st</h4>
+                </li>
+                <li className='order-card__order-item'>
+                    <h4 className='order-card__product-name'>produktnamn</h4>
+                    <h4 className='order-card__product-size'>size</h4>
+                    <h4 className='order-card__product-quantity'>2 st</h4>
+                </li>
+                <li className='order-card__order-item'>
+                    <h4 className='order-card__product-name'>produktnamn</h4>
+                    <h4 className='order-card__product-size'>size</h4>
+                    <h4 className='order-card__product-quantity'>2 st</h4>
+                </li>
+                <li className='order-card__order-item'>
+                    <h4 className='order-card__product-name'>produktnamn</h4>
+                    <h4 className='order-card__product-size'>size</h4>
+                    <h4 className='order-card__product-quantity'>2 st</h4>
+                </li>
+                <li className='order-card__order-item'>
+                    <h4 className='order-card__product-name'>produktnamn</h4>
+                    <h4 className='order-card__product-size'>size</h4>
+                    <h4 className='order-card__product-quantity'>2 st</h4>
+                </li>
+                <li className='order-card__order-item'>
+                    <h4 className='order-card__product-name'>produktnamn</h4>
+                    <h4 className='order-card__product-size'>size</h4>
+                    <h4 className='order-card__product-quantity'>2 st</h4>
+                </li>
+                <li className='order-card__order-item'>
+                    <h4 className='order-card__product-name'>produktnamn</h4>
+                    <h4 className='order-card__product-size'>size</h4>
+                    <h4 className='order-card__product-quantity'>2 st</h4>
+                </li>
+
+                {/* <h3 className='order-card__order-id'>{getLastNCharacters(order._id, 4)}</h3>
                 <h3 className='order-card__date'>{formatDate(order.createdAt)}</h3>
                 {order.order.map((orderItem) => (
                     <li className='order-card__order-item'>
@@ -39,7 +103,7 @@ const OrderCard: React.FC<Props> = ({ size, order }) => {
                         <h4 className='order-card__product-size'>{orderItem.size}</h4>
                         <h4 className='order-card__product-quantity'>{orderItem.quantity}</h4>
                     </li>
-                ))}
+                ))} */}
                 <TextButton onClick={() => console.log('Här var det klickat!')}>KLAR FÖR SERVERING</TextButton>
             </ul>
         );
--- business/src/pages/ChefPage/ChefPage.tsx
+++ business/src/pages/ChefPage/ChefPage.tsx
@@ -1,6 +1,31 @@
+import OrderCard from '../../components/OrderCard/OrderCard';
 import './chefPage.css';
 const ChefPage: React.FC = () => {
-    return <div>ChefPage</div>;
+    return (
+        <main className='chef-page'>
+            <section className='order-queue'>
+                <h2 className='order-queue__title'>Väntande Ordrar (8st)</h2>
+                <ul className='order-queue__order-list'>
+                    <OrderCard size='medium' />
+                    <OrderCard size='medium' />
+                    <OrderCard size='medium' />
+                    <OrderCard size='medium' />
+                    <OrderCard size='medium' />
+                    <OrderCard size='medium' />
+                    <OrderCard size='medium' />
+                    <OrderCard size='medium' />
+                    <OrderCard size='medium' />
+                    <OrderCard size='medium' />
+                    <OrderCard size='medium' />
+                    <OrderCard size='medium' />
+                </ul>
+            </section>
+            <section className='active-order'>
+                <h2 className='active-order__title'>Tillagas</h2>
+                <OrderCard size='large' />
+            </section>
+        </main>
+    );
 };
 
 export default ChefPage;
