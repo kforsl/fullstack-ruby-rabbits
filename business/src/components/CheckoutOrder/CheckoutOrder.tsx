37b29801f3498d176324af947bf260d5b1e0c10f
kforsl <forslund95@live.com>
2024-11-20 19:16:21 +0100

    implement useGetOrders querie

--- business/src/components/CheckoutOrder/CheckoutOrder.tsx
+++ business/src/components/CheckoutOrder/CheckoutOrder.tsx
@@ -1,4 +1,4 @@
-import { OrderType } from '../../interfaces/interfaceOrder';
+import { useGetOrders } from '../../services/queries';
 import OrderCard from '../OrderCard/OrderCard';
 import './checkoutOrder.css';
 
@@ -7,158 +7,30 @@ interface Props {
 }
 
 const CheckoutOrder = ({ changeview }: Props) => {
-    const orders: OrderType[] = [
-        {
-            _id: '0001',
-            price: 23,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            state: 'waiting',
-            order: [],
-        },
-        {
-            _id: '0002',
-            price: 23,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            state: 'waiting',
-            order: [],
-        },
-        {
-            _id: '0003',
-            price: 23,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            state: 'waiting',
-            order: [],
-        },
-        {
-            _id: '0004',
-            price: 23,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            state: 'waiting',
-            order: [],
-        },
-        {
-            _id: '0005',
-            price: 23,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            state: 'waiting',
-            order: [],
-        },
-        {
-            _id: '0006',
-            price: 23,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            state: 'waiting',
-            order: [],
-        },
-        {
-            _id: '0007',
-            price: 23,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            state: 'preparing',
-            order: [],
-        },
-        {
-            _id: '0008',
-            price: 23,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            state: 'preparing',
-            order: [],
-        },
-        {
-            _id: '0009',
-            price: 23,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            state: 'preparing',
-            order: [],
-        },
-        {
-            _id: '0010',
-            price: 23,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            state: 'preparing',
-            order: [],
-        },
-        {
-            _id: '0011',
-            price: 23,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            state: 'preparing',
-            order: [],
-        },
-        {
-            _id: '0012',
-            price: 23,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            state: 'preparing',
-            order: [],
-        },
-        {
-            _id: '0013',
-            price: 23,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            state: 'ready',
-            order: [],
-        },
-        {
-            _id: '0014',
-            price: 23,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            state: 'ready',
-            order: [],
-        },
-        {
-            _id: '0015',
-            price: 23,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            state: 'ready',
-            order: [],
-        },
-        {
-            _id: '0016',
-            price: 23,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            state: 'ready',
-            order: [],
-        },
-        {
-            _id: '0017',
-            price: 23,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            state: 'ready',
-            order: [],
-        },
-        {
-            _id: '0018',
-            price: 23,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            state: 'ready',
-            order: [],
-        },
-    ];
+    const { data, isLoading, isError, error } = useGetOrders();
+
+    if (isLoading) {
+        return (
+            <main className='chef-page'>
+                <p>Loading...</p>
+            </main>
+        );
+    }
+
+    if (isError) {
+        return (
+            <main className='chef-page'>
+                <p>{`${error}`}</p>
+            </main>
+        );
+    }
+
     return (
         <section className='checkoutOrder'>
             <article className='checkoutOrder__section'>
                 <h2 className='checkoutOrder__section-title'> Väntande Ordrar (11) </h2>
                 <ul className='checkoutOrder__order-list'>
-                    {orders.map(
+                    {data?.map(
                         (order) =>
                             order.state === 'waiting' && <OrderCard size={'small'} order={order} key={order._id} />
                     )}
@@ -167,7 +39,7 @@ const CheckoutOrder = ({ changeview }: Props) => {
             <article className='checkoutOrder__section'>
                 <h2 className='checkoutOrder__section-title'> Tillagas </h2>
                 <ul className='checkoutOrder__order-list'>
-                    {orders.map(
+                    {data?.map(
                         (order) =>
                             order.state === 'preparing' && <OrderCard size={'small'} order={order} key={order._id} />
                     )}
@@ -176,7 +48,7 @@ const CheckoutOrder = ({ changeview }: Props) => {
             <article className='checkoutOrder__section'>
                 <h2 className='checkoutOrder__section-title'> Redo att hämtas </h2>
                 <ul className='checkoutOrder__order-list'>
-                    {orders.map(
+                    {data?.map(
                         (order) => order.state === 'ready' && <OrderCard size={'small'} order={order} key={order._id} />
                     )}
                 </ul>
