3d352ff53e0e50d2083955b037a490d3bd95ac81
Magnus Nilsson <regnhundar@gmail.com>
2024-11-28 10:52:29 +0100

    Adding a mutate for each state switch

--- client/src/components/OrderConfirmation/OrderConfirmation.tsx
+++ client/src/components/OrderConfirmation/OrderConfirmation.tsx
@@ -12,12 +12,10 @@ interface Props {
 const OrderConfirmation: React.FC<Props> = ({ order }) => {
     const orderNumber = order ? getLastNCharacters(order._id, 4) : '';
     const { customer } = useAuthStore();
+    const { mutate: anullOrder, isPending: isAnulling, isSuccess: isAnulled } = useChangeOrderState();
+    const { mutate: editOrder, isPending: isChangingToEditState, isSuccess: isInEditState } = useChangeOrderState();
+    // const { mutate: backToWaiting, isPending: isChangingToWaitingState, isSuccess: isWaiting } = useChangeOrderState();
 
-    const { mutate: changeState, isPending, isSuccess: isAnulled } = useChangeOrderState();
-
-    const handleUpdateState = (id: string, state: 'waiting' | 'editing' | 'anulled') => {
-        changeState({ id, state });
-    };
     return (
         <article className='order-confirmation'>
             {isAnulled && <h2 className='order-confirmation__deleted'>ANNULERAD!</h2>}
@@ -38,17 +36,25 @@ const OrderConfirmation: React.FC<Props> = ({ order }) => {
                 {order.order.map((orderItem) => (
                     <li className='order-confirmation__order-list-item' key={orderItem._id}>
                         <h4 className='order-confirmation__list-info'>
-                            orderItem.product.name :({/* Behöver fixa så namnet är tillgängligt i objektet */}
+                            orderItem. product. name :({/* Behöver fixa så namnet är tillgängligt i objektet */}
                         </h4>
                         <h4 className='order-confirmation__list-info'>{orderItem.size}</h4>
                         <h4 className='order-confirmation__list-info'>{orderItem.quantity}</h4>
                     </li>
                 ))}
+                <h3 className='order-confirmation__category-title'>{`Totalpris: ${order.price}kr`}</h3>
             </ul>
             {!isAnulled && (
-                <TextButton onClick={() => handleUpdateState(order._id, 'anulled')}>
-                    {isPending ? 'Loading...' : 'AVBRYT ORDER'}
-                </TextButton>
+                <div className='order-confirmation__button-wrapper'>
+                    <TextButton onClick={() => anullOrder({ id: order._id, state: 'anulled' })} disabled={isAnulling}>
+                        {isAnulling ? 'Loading...' : 'AVBRYT ORDER'}
+                    </TextButton>
+                    <TextButton
+                        onClick={() => editOrder({ id: order._id, state: 'editing' })}
+                        disabled={isChangingToEditState}>
+                        {isChangingToEditState ? 'Loading...' : 'ÄNDRA ORDER'}
+                    </TextButton>
+                </div>
             )}
         </article>
     );
