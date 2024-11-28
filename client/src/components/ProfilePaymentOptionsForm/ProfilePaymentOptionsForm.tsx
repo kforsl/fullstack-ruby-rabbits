fa3106c4da303d5d83c2258161eb68711188e458
Jaerker <johanjakberger@gmail.com>
2024-11-28 13:30:19 +0100

    Fixed backend for payment options

--- client/src/components/ProfilePaymentOptionsForm/ProfilePaymentOptionsForm.tsx
+++ client/src/components/ProfilePaymentOptionsForm/ProfilePaymentOptionsForm.tsx
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import './profilePaymentOptionsForm.css';
 const ProfilePaymentOptionsForm = () => {
+    const [formData, setFormData] = useState({});
+
     return (
         <form className='payment-form'>
             <h2 className='payment-form__title'> Betalningsalternativ: </h2>
             <section className='payment-form__input-section'>
-                <label className='payment-form__label'>Betala med Swish?</label>
+                <label className='payment-form__label'>Swish</label>
+                <input className='payment-form__checkbox' type='checkbox' />
+            </section>
+            <section className='payment-form__input-section'>
+                <label className='payment-form__label'>Betalkort</label>
                 <input className='payment-form__checkbox' type='checkbox' />
             </section>
         </form>
