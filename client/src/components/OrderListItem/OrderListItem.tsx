55c8f96b6464804b9d8b641f9640f802a255fb69
Jaerker <johanjakberger@gmail.com>
2024-11-29 09:31:06 +0100

    fixed small bugs in order list

--- client/src/components/OrderListItem/OrderListItem.tsx
+++ client/src/components/OrderListItem/OrderListItem.tsx
@@ -11,7 +11,7 @@ const OrderListItem = ({ order }: Props) => {
             <Link className='order-list-item__link' to={`/orderbekraftelse/${order._id}`}>
                 {order._id.slice(order._id.length - 4, order._id.length).toLocaleUpperCase()}
             </Link>
-            {order.state !== 'history' && <p className='order-list-item__status'> Status: {order.state}</p>}
+            <p className='order-list-item__status'> Status: {order.state}</p>
             <p className='order-list-item__price'> {order.price} kr </p>
             <p className='order-list-item__date'> {new Date(order.createdAt).toDateString()} </p>
         </li>
