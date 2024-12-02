a28de57b083255b45e147e4624313cac2c1e8a70
kforsl <forslund95@live.com>
2024-12-02 14:25:14 +0100

    Created rooms for employee and order

--- server/server.js
+++ server/server.js
@@ -70,15 +70,24 @@ const run = async () => {
     try {
         await mongoose.connect(process.env.DB_URL);
         io.on('connection', (socket) => {
-            console.log('a user connected');
+            console.log('a user connected', socket.id);
+
             socket.on('disconnect', async (reason) => {
                 console.log(`USER DISCONNECTED: ${socket.id}. REASON: ${reason}`);
             });
-            socket.on('createOrder', () => {
-                io.emit('newOrder');
+            socket.on('joinEmployeeRoom', () => {
+                socket.join('employee');
+                console.log(socket.id, 'Is in room Employee');
             });
-            socket.on('updateOrderStatus', () => {
-                io.emit('newOrderStatus');
+            socket.on('joinOrderRoom', (id) => {
+                socket.join(id);
+                console.log(socket.id, 'Is in room ', id);
+            });
+            socket.on('createOrder', (id) => {
+                socket.to('employee').to(id).emit('newOrder');
+            });
+            socket.on('updateOrderStatus', (id) => {
+                socket.to('employee').to(id).emit('newOrderStatus');
             });
         });
         server.listen(3000, () => console.log(`Server started on PORT ${PORT}`));
