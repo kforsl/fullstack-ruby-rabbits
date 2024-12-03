2d40e490597110adde5dc70e7359e5a52b999ee8
kforsl <forslund95@live.com>
2024-12-03 10:39:18 +0100

    useSize.ts

--- client/src/components/NavigationMenu/NavigationMenu.tsx
+++ client/src/components/NavigationMenu/NavigationMenu.tsx
@@ -2,8 +2,12 @@ import { Link } from 'react-router-dom';
 import './navigationMenu.css';
 import Cart from '../Cart/Cart';
 import useAuthStore from '../../stores/authStore';
+import { useEffect, useState } from 'react';
+import useSize from '../../utils/useSize';
 const NavigationMenu: React.FC = () => {
     const { setIsShowingForm, customer } = useAuthStore();
+    const { windowWidth } = useSize();
+    const [isBurgerShowing, setIsBurgerShowing] = useState<boolean>(false);
 
     const navigation = [
         { name: 'MENU', route: '/' },
@@ -16,25 +20,35 @@ const NavigationMenu: React.FC = () => {
         else window.location.href = '/profil';
     };
 
+    useEffect(() => {
+        if (!isBurgerShowing) if (windowWidth < 500) setIsBurgerShowing(true);
+
+        if (isBurgerShowing) if (windowWidth > 500) setIsBurgerShowing(false);
+    }, [windowWidth]);
     return (
         <nav className='navigation-menu'>
-            <ul className='navigation-menu__list'>
-                {navigation.map((navigationItem) => (
-                    <li className='navigation-menu__list-item' key={navigationItem.route}>
-                        <Link to={navigationItem.route} className='navigation-menu__link'>
-                            {navigationItem.name}
-                        </Link>
-                    </li>
-                ))}
-                <li className='navigation-menu__list-item'>
-                    <button
-                        onClick={onProfileButtonClicked}
-                        className='navigation-menu__link navigation-menu__link--profile'>
-                        {customer !== null ? 'PROFIL' : 'LOGGA IN'}
-                    </button>
-                </li>
-            </ul>
-
+            {isBurgerShowing ? (
+                <ul> My Burger</ul>
+            ) : (
+                <>
+                    <ul className='navigation-menu__list'>
+                        {navigation.map((navigationItem) => (
+                            <li className='navigation-menu__list-item' key={navigationItem.route}>
+                                <Link to={navigationItem.route} className='navigation-menu__link'>
+                                    {navigationItem.name}
+                                </Link>
+                            </li>
+                        ))}
+                        <li className='navigation-menu__list-item'>
+                            <button
+                                onClick={onProfileButtonClicked}
+                                className='navigation-menu__link navigation-menu__link--profile'>
+                                {customer !== null ? 'PROFIL' : 'LOGGA IN'}
+                            </button>
+                        </li>
+                    </ul>
+                </>
+            )}
             <Cart />
         </nav>
     );
